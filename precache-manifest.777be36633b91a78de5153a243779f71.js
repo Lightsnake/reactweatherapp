fd2fd3c61cc969ddd150a8c33e239b821d6e1935
Lightsnake <andrew.tsan@yahoo.com>
2019-08-09 15:53:03 -0400

    Updates

---
+++ precache-manifest.777be36633b91a78de5153a243779f71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d9d80a9d379a778886bd8ade8f272a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c26fef77d649d0d09c0",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "44aa23b984ba9900ec51",
+    "url": "/static/css/main.5572b3b2.chunk.css"
+  },
+  {
+    "revision": "0c26fef77d649d0d09c0",
+    "url": "/static/js/2.f1e50439.chunk.js"
+  },
+  {
+    "revision": "44aa23b984ba9900ec51",
+    "url": "/static/js/main.94c11fae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  }
+]);
